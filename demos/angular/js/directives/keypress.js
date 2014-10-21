21866cb7afed3a3826f5752e83db899eea3f8552
marcysutton <holla@marcysutton.com>
2014-10-21 09:24:05 +0200

    Adding live region example

--- demos/angular/js/directives/keypress.js
+++ demos/angular/js/directives/keypress.js
@@ -14,15 +14,16 @@ myStuff.directive('ngEnter', function () {
   return function (scope, element, attrs) {
     element.bind("keydown keypress", function (event) {
       if(event.which === 40 || event.which === 38) {
-        scope.$apply(function (){
-          scope.$eval(attrs.ngArrow);
-        });
+        // scope.$apply(function (){
+        //   scope.$eval(attrs.ngArrow);
+        // });
         if(event.which === 40){
           scope.$index++;
         }
         if(event.which === 38){
           scope.$index--;
         }
+        console.log(scope.$index);
         event.preventDefault();
       }
     })
